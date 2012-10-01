4cc2b2817b04338af82fc756a0f731bfd0bd9274
unknown <kuboyama@kuboyama-dt112.camobile.com>
2012-10-01 13:00:42 +0900

    mon

--- app/services/YahooLocalSearch.scala
+++ app/services/YahooLocalSearch.scala
@@ -18,9 +18,10 @@ object YahooLocalSearch {
     println("reached yls get method")
     println(args.apply("query"))
 
-    // 4136755b3cd84823420aec02ac373152 : yahooロコ
+    // 4136755b3cd84823420aec02ac373152 : 町ログ
     // cd246abc0da32d9d0e4990a54e3bf0e4 : 全国寺社ガイド
     // bf1929f6ab23005e9b80adf4fd0fc5ab : なんかママ向けのカフェとかそういうやつ
+    // 132c700e0ebf5a1d386e9b725d7d1e6b : yahoo ロコ
 
     var cid = ""
     val random = new Random
@@ -28,18 +29,20 @@ object YahooLocalSearch {
     seed += ((random.nextInt(10)).toString)
 
     // ランダムにカセットを切り替える
-    if (Integer.parseInt(seed) % 3 == 0) {
+    if (Integer.parseInt(seed) % 4 == 0) {
       cid = "4136755b3cd84823420aec02ac373152"
-    } else if (Integer.parseInt(seed) % 3 == 1) {
+    } else if (Integer.parseInt(seed) % 4 == 1) {
       cid = "cd246abc0da32d9d0e4990a54e3bf0e4"
-    } else {
+    } else if (Integer.parseInt(seed) % 4 == 2) {
       cid = "bf1929f6ab23005e9b80adf4fd0fc5ab"
+    } else {
+      cid = "132c700e0ebf5a1d386e9b725d7d1e6b"
     }
     
     val random_2 = new Random
     var seed_2 = new String
-    seed += ((random.nextInt(10)).toString)
-    var offset = (Integer.parseInt(seed)) % 10
+    seed_2 += ((random_2.nextInt(10)).toString)
+    var offset = (Integer.parseInt(seed_2)) % 3
 
     val res = WS.url(API_URL)
       .setQueryParameter("appid", API_KEY)
